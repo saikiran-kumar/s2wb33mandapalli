96b99e4ae92fc79b2be727be5910501f77f7f81f
saikiran-kumar <saikirankumar34@gmail.com>
2022-04-04 18:21:50 -0500

    added

--- routes/addmods.js
+++ routes/addmods.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/req.query.foo', function(req, res, next) {
+router.get('/', function(req.query, res, next) {
   
   res.render('addmods', { title:'addmods', query: query});
   let query = req.query
