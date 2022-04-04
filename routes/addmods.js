915d1330722abb6c8de729518576c7b46803c12c
saikiran-kumar <saikirankumar34@gmail.com>
2022-04-04 17:27:31 -0500

    Update addmods.js

--- routes/addmods.js
+++ routes/addmods.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  req.query.foo;
-  res.render('addmods', { title: 'Addmods', query: query});
+router.get('/req.query.foo', function(req, res, next) {
+  
+  res.render('addmods', { title:'addmods', query: query});
   let query = req.query
   console.log(`rows ${query.rows}`)
   console.log(`cols ${query.cols}`)
