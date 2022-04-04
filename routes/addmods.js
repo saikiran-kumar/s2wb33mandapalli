07f98c9e5f925a6912a22e6e29a1d72002a09825
saikiran-kumar <saikirankumar34@gmail.com>
2022-04-04 16:41:45 -0500

    Create addmods.js

---
+++ routes/addmods.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('addmods', { title: 'Addmods', query: query});
+  let query = req.query
+  console.log(`rows ${query.rows}`)
+  console.log(`cols ${query.cols}`)
+});
+
+module.exports = router;
